d08d78f7161f8d8a2dc43c75d3b7c174656d77ff
Umme-hanii <ummehanimohammed9@gmail.com>
2023-05-09 17:23:55 +0530

    Creating Modules for client and company

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientModule } from './clients/client.module';
+import { CompanyModule } from './company/company.module';
 import { ClientPageComponent } from './clients/client-page/client-page.component';
 import { SearchFormComponent } from './search/search-form/search-form.component';
 import { ClientFormComponent } from './clients/client-form/client-form.component';
@@ -21,7 +23,9 @@ import { ClientFormComponent } from './clients/client-form/client-form.component
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ClientModule,
+    CompanyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
