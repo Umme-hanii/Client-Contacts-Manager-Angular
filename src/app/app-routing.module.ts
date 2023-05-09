777d1402a3352badfdb01d063f93b19280286d47
Umme-hanii <ummehanimohammed9@gmail.com>
2023-05-09 20:42:22 +0530

    Add Company Page component and route to the application.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientPageComponent } from './clients/client-page/client-page.component';
+import { CompanyPageComponent } from './company/company-page/company-page.component';
 
 const routes: Routes = [
   {
     path: 'clients',
     component: ClientPageComponent
+  },
+  {
+    path: 'company',
+    component: CompanyPageComponent
   },
   {
     path: '',
